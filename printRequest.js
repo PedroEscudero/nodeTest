356a3ef1d26751e6553c00a79a78657c08ebcdde
pedroescudero <pedroescudero.zumel@gmail.com>
2015-03-23 00:48:37 +0100

    printRequest

--- printRequest.js
+++ printRequest.js
@@ -1,14 +1,17 @@
 var http = require ('http');
 http.createServer(function(request, response){
 	response.writeHead(200);
-	request.on('readeble', function(){
+	request.on('readable', function(){
 		var chunk = null;
-		while (null !== (chunk = request.red())){
-			console.log(chunk.toString);
+		while (null !== (chunk = request.read())){
+			console.log(chunk.toString());
+			response.write(chunk);
 		}
+		console.log('readable');
 	});
+	console.log('Call to server');
 	request.on('end', function(){
 		response.end();
 	});
-}).listen(8078);
-console.log ('Print Request - listening on 8078...');
+}).listen(8080);
+console.log ('Print Request - listening on 8080...');
