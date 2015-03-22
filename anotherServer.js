a358e27dbe6d4bac1dd4aec01bd348ec15451d26
pedroescudero <pedroescudero.zumel@gmail.com>
2015-03-22 23:35:49 +0100

    fixing anotherServer

--- anotherServer.js
+++ anotherServer.js
@@ -1,7 +1,8 @@
+var http = require('http');
 var server = http.createServer();
 server.on('request', function(request, response){
 	response.writeHead(200);
 	response.write('Server initialized')
 	response.end();
-}).port(8079);
+}).listen(8079);
 console.log ('Listening on 8079');
