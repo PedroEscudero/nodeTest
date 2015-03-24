6a612c402dab69da6fe331c710b08a4dcfff3ca2
Pedro Escudero <pedroescudero.zumel@gmail.com>
2015-03-24 23:01:44 +0100

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-http.createServer(function(request, response){
-
-	response.writeHead(200);
-	response.write("Hello, this is dog.");
-	response.end();
-}).listen(8080);
-console.log('Listening on port 8080...');
-
