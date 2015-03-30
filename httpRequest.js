c48bf531bbd00b8cf93a0e4ef9f5e364b694f012
pedroescudero <pedroescudero.zumel@gmail.com>
2015-03-30 23:16:27 +0200

    fix error with request

--- httpRequest.js
+++ httpRequest.js
@@ -1,13 +1,13 @@
-var http = requier ('http');
+var http = require ('http');
 var message = "Go, go, go!";
 var options = {
 	host: 'localhost',
 	port: 8080,
-	path '/',
+	path: '/',
 	method: 'POST'
 }
 
-var request = http.request(options, functions(response){
+var request = http.request(options, function(response){
 	response.on('data', function(data){
 		console.log(data);
 	});
