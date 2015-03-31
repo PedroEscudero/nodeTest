5461d9166d4f64917e079ee397392c038044b81f
pedroescudero <pedroescudero.zumel@gmail.com>
2015-03-31 22:10:04 +0200

    webserver improves

--- webReader.js
+++ webReader.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
-fs.readFile('README.md', function (err, web) {
+fs.readFile('README.md', function (err, web2) {
     if (err) {
         throw err; 
-    }       
+    }
+    var web = '<a href="http://google.es">Lets google</a>';
     http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(web);  
