5b9acf3b74fdb8819fa2a91d756cd5a04ff17400
Pedro Escudero <pedroescudero.zumel@gmail.com>
2015-03-31 09:34:51 +0200

    webReader.js
    Reader for display any html file

---
+++ webReader.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+var fs = require('fs');
+
+fs.readFile('./index.html', function (err, web) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(web);  
+        response.end();  
+    }).listen(8080);
+    
+});
